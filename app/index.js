a140616eb6f97b28f874d461c744dc9f8ea7e6bf
tarinOfficial <taringunarathna25@gmail.com>
2024-12-01 15:01:28 +0530

    Finished index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import AnimatedSplash from 'react-native-animated-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import AnimatedSplash from "react-native-animated-splash-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { Image } from "expo-image";
+import { Link } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [isAppLoaded, setIsAppLoaded] = useState(false);
@@ -9,7 +16,8 @@ export default function App() {
 
   const [loaded, error] = useFonts({
     "Sen-extraBold": require("../assets/fonts/Sen-ExtraBold.ttf"),
-    "Sen-regular": require("../assets/fonts/Sen-Regular.ttf")
+    "Sen-regular": require("../assets/fonts/Sen-Regular.ttf"),
+    "Sen-bold": require("../assets/fonts/Sen-Bold.ttf")
   });
 
   useEffect(
@@ -29,15 +37,40 @@ export default function App() {
     <AnimatedSplash
       translucent={true}
       isLoaded={isAppLoaded}
-      logoImage={require('../assets/images/logo.png')}
-      backgroundColor={'#f3f4f6'}
+      logoImage={require("../assets/images/logo.png")}
+      backgroundColor={"#f3f4f6"}
       logoHeight={150}
       logoWidth={150}
     >
       {/* App Content */}
-      <View style={styles.container}>
-        <Text style={styles.text}>Welcome to Foodie App!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/images/getStartedPage/imageOne.png")}
+            style={styles.imagePlaceholder}
+          />
+        </View>
+        <Text style={styles.title}>All your favorites</Text>
+        <Text style={styles.subtitle}>
+          Get all your loved foods in one once place, you just place the order
+          we do the rest
+        </Text>
+        <View style={styles.pagination}>
+          <View style={[styles.dot, styles.activeDot]} />
+          <View style={styles.dot} />
+          <View style={styles.dot} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Link href="/onBoardTwo" style={styles.nextButton}>
+            <TouchableOpacity style={{ width: "100%" , height: "100%", justifyContent: "center", alignItems: "center"}}>
+              <Text style={styles.nextButtonText}>NEXT</Text>
+            </TouchableOpacity>
+          </Link>
+          <Link href="/onBoardThree" style={styles.skipContainer}>
+    <Text style={styles.skipText}>Skip</Text>
+  </Link>
+        </View>
+      </SafeAreaView>
     </AnimatedSplash>
   );
 }
@@ -45,13 +78,77 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f3f4f6',
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#2d3748',
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imagePlaceholder: {
+    width: 350,
+    height: 350,
+    marginTop: -70
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: "Sen-extraBold",
+    textAlign: "center",
+    marginTop: -10
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#646982",
+    marginVertical: 20,
+    fontFamily: "Sen-regular"
+  },
+  pagination: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 60
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "#FFE1CE",
+    marginHorizontal: 5
+  },
+  activeDot: {
+    backgroundColor: "#FF7622"
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 30,
+    width: "100%",
+    alignItems: "center"
+  },
+  nextButton: {
+    backgroundColor: "#FF7622",
+    paddingVertical: 20,
+    paddingHorizontal: 80,
+    borderRadius: 12,
+    marginBottom: 15,
+    width: "100%"
+  },
+  nextButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+    fontFamily: "Sen-bold"
+  },
+  skipContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  skipText: {
+    fontSize: 16,
+    color: "#646982",
+    fontFamily: "Sen-regular",
   },
 });
